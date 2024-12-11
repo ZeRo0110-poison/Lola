17449c567aadb03992cd64fb44039dc422e301ad
ZeRo0110-poison <dszr0110@gmail.com>
2024-12-11 12:29:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('nuevaReceta').addEventListener('click', () => {
+    // Aquí puedes crear la función para mostrar el modal con los campos correspondientes a la receta
+});
+
+// Aquí puedes crear la función para guardar las recetas en el localStorage
+let bdata = window.localStorage;
+
+// Aquí puedes crear la función para renderizar las recetas en la página inicial
